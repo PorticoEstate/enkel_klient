cbf0bad1f6e0db26bab9e302f745fc0420815728
Sigurd Nes <sigurdne@online.no>
2022-04-08 11:53:19 +0200

    change to relative url

--- src/lib/functions.php
+++ src/lib/functions.php
@@ -2,7 +2,8 @@
 
 	function current_site_url()
 	{
-		$page_url = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://{$_SERVER['HTTP_HOST']}" . pathinfo($_SERVER['SCRIPT_NAME'], PATHINFO_DIRNAME) . '/';
+//		$page_url = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://{$_SERVER['HTTP_HOST']}" . pathinfo($_SERVER['SCRIPT_NAME'], PATHINFO_DIRNAME) . '/';
+		$page_url = pathinfo($_SERVER['SCRIPT_NAME'], PATHINFO_DIRNAME) . '/';
 		return $page_url;
 	}
 

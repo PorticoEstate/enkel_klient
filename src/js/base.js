fc248c952c025d176e052ae0cda2ee82dd7ee7b0
Sigurd Nes <sigurdne@gmail.com>
2025-06-03 15:58:46 +0200

    refactor: Improve URL handling functions by adding encoding and query string building utilities

--- src/js/base.js
+++ src/js/base.js
@@ -6,36 +6,86 @@
  */
 
 /**
+ * Safely encode URL parameters
+ * @param {string} value - The value to encode
+ * @returns {string} Encoded value
+ */
+function encodeUrlParam(value) {
+    return encodeURIComponent(value || '');
+}
+
+/**
+ * Convert object to URL query string
+ * @param {Object} params - Parameters object
+ * @returns {string} Query string
+ */
+function buildQueryString(params) {
+    if (!params || typeof params !== 'object') {
+        return '';
+    }
+    
+    const pairs = [];
+    for (const key in params) {
+        if (params.hasOwnProperty(key) && params[key] !== null && params[key] !== undefined) {
+            pairs.push(encodeUrlParam(key) + '=' + encodeUrlParam(params[key]));
+        }
+    }
+    return pairs.join('&');
+}
+
+/**
+ * Build complete URL with base URL, path and parameters
+ * @param {string} baseUrl - Base URL
+ * @param {string} path - URL path
+ * @param {Object} params - Query parameters
+ * @returns {string} Complete URL
+ */
+function buildUrl(baseUrl, path, params) {
+    if (!baseUrl) {
+        throw new Error('Base URL is required');
+    }
+    
+    const urlParts = baseUrl.split('?');
+    let newUrl = urlParts[0] + (path || '');
+    
+    // Combine existing query params with new ones
+    const allParams = {};
+    
+    // Parse existing query string
+    if (urlParts[1]) {
+        const existingParams = new URLSearchParams(urlParts[1]);
+        for (const [key, value] of existingParams) {
+            allParams[key] = value;
+        }
+    }
+    
+    // Add new parameters
+    if (params) {
+        Object.assign(allParams, params);
+    }
+    
+    const queryString = buildQueryString(allParams);
+    return queryString ? newUrl + '?' + queryString : newUrl;
+}
+
+/**
  * Emulate phpGW's link function
  *
- * @param String strURL target URL
- * @param Object oArgs Query String args as associate array object
- * @param bool bAsJSON ask that the request be returned as JSON (experimental feature)
- * @returns String URL
+ * @param {string} strURL target URL
+ * @param {Object} oArgs Query String args as associate array object
+ * @param {boolean} bAsJSON ask that the request be returned as JSON (experimental feature)
+ * @returns {string} URL
  */
-function phpGWLink(strURL, oArgs, bAsJSON)
-{
-	var arURLParts = strBaseURL.split('?');
-	var strNewURL = arURLParts[0] + strURL + '?';
-
-	if (oArgs == null)
-	{
-		oArgs = new Object();
-	}
-
-	for (obj in oArgs)
-	{
-		strNewURL += obj + '=' + oArgs[obj] + '&';
-	}
-
-	if (typeof (arURLParts[1]) !== 'undefined')
-	{
-		strNewURL += arURLParts[1];
-	}
-
-	if (bAsJSON)
-	{
-		strNewURL += '&phpgw_return_as=json';
-	}
-	return strNewURL;
+function phpGWLink(strURL, oArgs, bAsJSON) {
+    if (typeof strBaseURL === 'undefined') {
+        throw new Error('strBaseURL is not defined');
+    }
+    
+    const params = oArgs ? { ...oArgs } : {};
+    
+    if (bAsJSON) {
+        params.phpgw_return_as = 'json';
+    }
+    
+    return buildUrl(strBaseURL, strURL, params);
 }

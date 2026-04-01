aae43b426f9cc66cf72f126cea91a3b3fdc8fa40
Sigurd Nes <sigurdne@gmail.com>
2026-04-01 11:46:20 +0200

    test

--- src/Controller/BaseFormController.php
+++ src/Controller/BaseFormController.php
@@ -102,7 +102,7 @@ abstract class BaseFormController
             $ssn = $oidc_claim_pid;
         }
         echo '<pre>';
-        print_r($headers);
+        print_r($_SERVER);
         echo '</pre>';
         ApiClient::session_set('common', 'ssn', $ssn);
 
